32465b8e4a5bfc540b8541a027e6110eba1f36fb
Juan-Gines <JuangAlenta@hotmail.com>
2022-08-24 14:54:50 +0200

    Terminados ejercicios tema 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cliente cliente = new Cliente();
+        cliente.edad=25;
+        cliente.nombre="Pablo";
+        cliente.telefono="5555555";
+        cliente.credito=5000;
+        System.out.println("El cliente "+cliente.nombre+" tiene "+cliente.edad+" años de edad, su teléfono es "+cliente.telefono+" y su crédito es "+cliente.credito+".");
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.edad=28;
+        trabajador.nombre="Román";
+        trabajador.telefono="5555556";
+        trabajador.salario=1000;
+        System.out.println("El trabajador "+trabajador.nombre+" tiene "+trabajador.edad+" años de edad, su teléfono es "+trabajador.telefono+" y su salario es "+trabajador.salario+".");
+
     }
+}
+
+class Persona{
+    int edad;
+    String nombre;
+    String telefono;
+}
+
+class Cliente extends Persona{
+    int credito;
+}
+
+class Trabajador extends Persona{
+    int salario;
 }
